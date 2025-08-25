b53b9502e6d52b70d85f55c33dcb9d0df49a4418
rainboiiiiii <thatsmaboye@gmail.com>
2025-08-25 00:40:48 -0400

    ...

--- Utilities/GitManager.cs
+++ Utilities/GitManager.cs
@@ -11,7 +11,7 @@ namespace TheCloud.Utilities
 {
     public static class GitManager
     {
-        private const string RepoPath = @"C:\Users\user\CloudLive";
+        private const string RepoPath = @"C:\Users\user\Desktop\CloudLive";
         private static JSONStructure config => AdminCommands.GetConfig();
 
         public static async Task<string> GetLatestCommitHashAsync()
