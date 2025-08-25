b4fdb0527d5b4c4af9b86f4698fc6dde8b700d23
rainboiiiiii <thatsmaboye@gmail.com>
2025-08-25 14:42:28 -0400

    Added code logic to work with updated commands

---
+++ DSharpPlus/DiscordWebBuilder.cs
@@ -0,0 +1,17 @@
+﻿using DSharpPlus.Entities;
+using System;
+
+namespace DSharpPlus
+{
+    internal class DiscordWebBuilder
+    {
+        public DiscordWebBuilder()
+        {
+        }
+
+        internal DiscordInteractionResponseBuilder AddEmbed(DiscordEmbedBuilder embed)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
