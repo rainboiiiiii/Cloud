21b2eeeeb7498f50e2cdc3be27ca04c3f8275707
rainboiiiiii <thatsmaboye@gmail.com>
2025-08-26 15:46:20 -0400

    added new response logic and responses

--- Responses/responses.cs
+++ Responses/responses.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace TheCloud.Listener
 {
+
     public static class BotResponses
+       
     {
 
 
@@ -60,6 +63,82 @@ namespace TheCloud.Listener
 
 
 
+        };
+
+        public static List<string> KilometerResponses(string username) => new()
+        {
+            $"A kilometer is 1,000 meters, or about 0.621 miles for my imperial friends. A kilometer is just a really long meter, {username}.",
+        };
+
+        public static List<string> HelpResponses(string username) => new()
+        {
+
+            $"Hey there, {username}! I currently do not possess the power nor knowledge to do live support just yet. However, feel free to open a ticket in <1148356467498438718> and a staff member will be happy to assist!"
+            
+        };
+
+        public static List<string> FreeGpResponses(string username) => new()
+        {
+            $"Free GP is a scam, {username}. Don't fall for it!",
+            $"There's no such thing as free GP, {username}. If it sounds too good to be true, it probably is.",
+            $"Free GP? More like free trouble, {username}. Stay away from that!",
+            $"Free GP is a trap, {username}. Don't get caught in it!",
+            $"Free GP is a myth, {username}. Don't waste your time looking for it!",
+            $"Absolutely not, {username}."
+
+        };
+
+        public static List<string> FreeBPResponses(string username) => new()
+        {
+            $"Free BP is a scam, {username}. Don't fall for it!",
+            $"There's no such thing as free BP, {username}. If it sounds too good to be true, it probably is.",
+            $"Free BP? More like free trouble, {username}. Stay away from that!",
+            $"Free BP is a trap, {username}. Don't get caught in it!",
+            $"Free BP is a myth, {username}. Don't waste your time looking for it!",
+            $"Absolutely not, {username}."
+        };
+
+        public static List<string> HanniResponses(string username) => new()
+        {
+            $"I have never met Hanni, but it would be nice to meet her one day,{username}.",
+            $"I heard Hanni is the GOAT, but I have not met her before, {username}.",
+            $"Hanni is cool from what I have heard, but I have never met her.",
+        };
+
+        public static List<string> MaintResponses(string username) => new()
+        {
+            $"We always notify users in advance about scheduled maintenance through our official channels.If you experience any issues after maintenance, please reach out to our support team for assistance",
+
+        };
+
+        public static List<string> OwnerResponses(string username) => new()
+        {
+            $"The owner is a cool person, {username}.",
+            $"I hear the owner is pretty awesome, {username}.",
+            $"The owner is the best, {username}.",
+            $"I have heard great things about the owner, {username}.",
+            $"The owner is a legend, {username}.",
+            $"The owner is a rockstar, {username}.",
+            $"The owner is a genius, {username}.",
+            $"The owner is a visionary, {username}.",
+            $"The owner is a hero, {username}.",
+            $"The owner is a superstar, {username}.",
+            $"The owner is a mastermind, {username}.",
+            $"The owner is a trailblazer, {username}.",
+            $"The owner is a pioneer, {username}.",
+            $"The owner is a game-changer, {username}.",
+            $"The owner is a force to be reckoned with, {username}."
+        };
+
+        public static List<string> CloudHelpResponses(string username) => new()
+        {
+            $"Thank you for wanting to help support my development! Use the /info command and look for the PayPal Link hyperlink and donate there!"
+        };
+
+        public static List<string> FreeStuffResponses(string username) => new()
+        {
+            $"Rain cannot send anything, Spanky and Macie only send gifts usually after maintenance or special events. Please do not ask for free stuff, {username}.",
+            $"Absolutely not, {username}."
         };
     }
 }
